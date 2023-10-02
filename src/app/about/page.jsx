2cdc20b7b6ba23270ba4c3ab87cd5205bdb591c5
Samuel Gómez <sagomadev@gmail.com>
2023-10-02 17:18:43 +0200

    Add About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,46 @@
+function AbautPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold my-4">About</h1>
+      <p className="text-gray-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        veritatis consectetur, minus doloribus mollitia temporibus quo alias
+        labore voluptas distinctio repellat! Vero harum architecto quas beatae
+        repudiandae omnis nobis commodi dolor cupiditate eius officiis nemo rem,
+        quis eligendi impedit, error, at nisi laudantium voluptas reiciendis
+        doloribus rerum! Cum asperiores a eum culpa repellat ad ipsa unde iste,
+        exercitationem id! Mollitia inventore fugit quam ipsa ab reprehenderit
+        voluptates. Dolorem, odio ullam. Repudiandae voluptatem rerum blanditiis
+        cumque, nemo quisquam fugit sunt veritatis accusamus laborum dolores!
+        Provident eligendi quo, blanditiis ducimus sint illum at veritatis
+        quibusdam repudiandae voluptas fuga praesentium, labore quidem
+        temporibus! Deleniti doloribus nesciunt iusto sed incidunt fugit iste
+        dolorem nobis deserunt, sit ullam molestias excepturi rem odio nostrum,
+        non animi dolorum? Laboriosam molestiae harum quam voluptatibus, enim
+        voluptas repellendus dicta commodi rerum. Nesciunt itaque laudantium
+        maxime omnis fugiat nulla debitis quibusdam aliquam amet quo magni
+        voluptates rerum mollitia ratione, repellat eos recusandae modi nobis
+        corporis totam at repudiandae praesentium doloremque! Modi iure optio
+        numquam vero obcaecati corporis itaque aliquam? Mollitia assumenda eum
+        tenetur omnis! Perferendis fugiat nihil minus provident exercitationem
+        recusandae nostrum distinctio magnam, libero perspiciatis eos aliquam
+        delectus, eveniet harum aliquid molestias possimus nobis facilis
+        ratione? Officiis aliquid minima labore repudiandae quas numquam eos
+        soluta fugit nostrum corrupti, libero aliquam nisi consectetur
+        blanditiis esse quod dignissimos laboriosam! Voluptas velit at
+        exercitationem mollitia! Delectus accusamus quasi a nisi deserunt
+        voluptatibus ad cupiditate consequatur suscipit iste porro natus, quia
+        labore minima earum? Nesciunt consequatur illo odio voluptatem
+        consectetur placeat vel labore accusamus eum quibusdam, harum
+        exercitationem quasi perspiciatis blanditiis ipsam inventore qui optio.
+        Asperiores, adipisci nulla dicta excepturi reprehenderit officia esse
+        tenetur rem, libero omnis autem sed aliquid voluptatum id magnam
+        voluptatibus. Magnam, error aspernatur dolore nam quos itaque harum
+        repellat quia sapiente cupiditate provident dolorem ipsam. Est iusto
+        eius aperiam.
+      </p>
+    </div>
+  );
+}
+
+export default AbautPage;
